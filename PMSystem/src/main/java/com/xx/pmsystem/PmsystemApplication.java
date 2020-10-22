68b1cd066967958e591c83853181ec7b0e6588ac
Stephen <xxw11165@gmail.com>
2020-10-22 00:12:38 -0700

    initial project

---
+++ PMSystem/src/main/java/com/xx/pmsystem/PmsystemApplication.java
@@ -0,0 +1,13 @@
+package com.xx.pmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsystemApplication.class, args);
+    }
+
+}
---
+++ PMSystem/src/main/java/com/xx/pmsystem/domain.java
@@ -0,0 +1,4 @@
+package com.xx.pmsystem;
+
+public class domain {
+}
