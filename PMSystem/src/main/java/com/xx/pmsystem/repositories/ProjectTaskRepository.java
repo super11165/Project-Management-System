2173f324b7445382c7c9b8200e1a732ccef6b834
Stephen <xxw11165@gmail.com>
2020-10-24 14:17:10 -0700

    add backlog && projectTask entities, set up project-backlog one to ont relationship, backlog- projectTasks one to many relationships

---
+++ PMSystem/src/main/java/com/xx/pmsystem/repositories/ProjectTaskRepository.java
@@ -0,0 +1,10 @@
+package com.xx.pmsystem.repositories;
+
+import com.xx.pmsystem.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+
+}
--- PMSystem/src/main/java/com/xx/pmsystem/services/ProjectService.java
+++ PMSystem/src/main/java/com/xx/pmsystem/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.xx.pmsystem.services;
 
+import com.xx.pmsystem.domain.Backlog;
 import com.xx.pmsystem.domain.Project;
 import com.xx.pmsystem.exceptions.ProjectIdException;
+import com.xx.pmsystem.repositories.BacklogRepository;
 import com.xx.pmsystem.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,22 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project){
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if(project.getId() == null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+            if(project.getId() != null){
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
             return projectRepository.save(project);
         }catch (Exception e){
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists!");
