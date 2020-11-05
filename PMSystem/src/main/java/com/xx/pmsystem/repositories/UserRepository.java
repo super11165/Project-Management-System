0506981c6552cc510277ff17a81bfbdfa7a319e6
Stephen <xxw11165@gmail.com>
2020-11-05 12:21:01 -0800

    JWT provider pre-word, generate token

--- PMSystem/src/main/java/com/xx/pmsystem/repositories/UserRepository.java
+++ PMSystem/src/main/java/com/xx/pmsystem/repositories/UserRepository.java
@@ -4,7 +4,12 @@ import com.xx.pmsystem.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+    User getById(Long id);
 
 }
--- PMSystem/src/main/java/com/xx/pmsystem/security/SecurityConfig.java
+++ PMSystem/src/main/java/com/xx/pmsystem/security/SecurityConfig.java
@@ -1,12 +1,21 @@
 package com.xx.pmsystem.security;
 
+import com.xx.pmsystem.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static com.xx.pmsystem.security.SecurityConstants.H2_URL;
+import static com.xx.pmsystem.security.SecurityConstants.SIGN_UP_URLS;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +28,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -43,7 +69,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 )
                 .permitAll()
-                .antMatchers("/api/users/**").permitAll()
+                .antMatchers(SIGN_UP_URLS).permitAll()
+                .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
     }
 }
