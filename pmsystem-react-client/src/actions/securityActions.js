65fd8fe0a65d11ddf132e0bfaf3685b92875136b
Stephen <xxw11165@gmail.com>
2020-11-06 22:47:27 -0800

    set securityAction secutiryReducer, login, login logic

--- pmsystem-react-client/src/actions/securityActions.js
+++ pmsystem-react-client/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async (dispatch) => {
   try {
@@ -8,6 +10,37 @@ export const createNewUser = (newUser, history) => async (dispatch) => {
     dispatch({
       type: GET_ERRORS,
       payload: {},
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });
+  }
+};
+
+export const login = (LoginRequest) => async (dispatch) => {
+  try {
+    //post => Login request
+
+    const res = await axios.post("/api/users/login", LoginRequest);
+
+    //extract token from res.data
+    const { token } = res.data;
+
+    //store the token in the local storage
+    localStorage.setItem("jwtToken", token);
+
+    //set our token in header ***
+    setJWTToken(token);
+
+    //decode token on react
+    const decoded = jwt_decode(token);
+
+    //dispatch to our securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded,
     });
   } catch (err) {
     dispatch({
